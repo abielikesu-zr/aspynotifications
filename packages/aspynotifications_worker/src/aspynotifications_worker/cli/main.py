9cbe04bba8cacf0263015c9da2d4456b3270d5a6
Abidis Solano <abidis@zeroramp.com>
2026-08-14 22:10:44 +0100

    merging

---
+++ packages/aspynotifications_worker/src/aspynotifications_worker/cli/main.py
@@ -0,0 +1,23 @@
+import click
+from aspyevents_worker.cli.worker import worker_start_command
+from aspynotifications_worker import get_notifications_worker
+from aspynotifications_worker.runner.notifications_worker_runner import (
+    NotificationsWorkerRunner,
+)
+
+
+@click.group()
+def cli() -> None:
+    """Aspy Notifications Worker CLI."""
+
+
+cli.add_command(
+    worker_start_command(
+        runner=NotificationsWorkerRunner,
+        worker_factory=get_notifications_worker,
+    )
+)
+
+
+if __name__ == "__main__":
+    cli()
