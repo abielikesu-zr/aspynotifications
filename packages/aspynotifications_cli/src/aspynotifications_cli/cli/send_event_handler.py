a6fd4abd4f11ebbfef705b7b31a5080b45ccd9cc
Abidis Solano <abidis@zeroramp.com>
2026-08-20 21:37:14 +0100

    Fautconfig of cli and worker

--- packages/aspynotifications_cli/src/aspynotifications_cli/cli/send_event_handler.py
+++ packages/aspynotifications_cli/src/aspynotifications_cli/cli/send_event_handler.py
@@ -1,10 +1,11 @@
 import json
-import structlog
 
+import structlog
 from aspylogger.services.logging_setup import configure_logging
-from aspynotifications_cli import load_aspynotifications_cli_config
 from aspynotifications_dtos.notify_request import CreateNotifyRequest
 from aspynotifications_sdk import get_notifications_sdk
+
+from aspynotifications_cli import load_aspynotifications_cli_config
 
 logger = structlog.get_logger(__name__)
 
@@ -14,6 +15,7 @@ async def send_event_handler(file_path: str, output_format: str) -> None:
 
     load_aspynotifications_cli_config()
     configure_logging()
+
     with open(file_path, "r", encoding="utf-8") as f:
         data = json.load(f)
 
@@ -21,4 +23,4 @@ async def send_event_handler(file_path: str, output_format: str) -> None:
     sdk = get_notifications_sdk()
     result = await sdk.notify(request)
     log.info("send_event_handler")
-    print('Server: ', result)
+    print("Server: ", result)
