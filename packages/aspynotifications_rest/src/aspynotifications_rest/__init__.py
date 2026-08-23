caa3242272ac5cd393468b3d95bfe02737109bed
Abidis Solano <abidis@zeroramp.com>
2026-08-23 18:33:04 +0100

    removed load that is no longer needed

--- packages/aspynotifications_rest/src/aspynotifications_rest/__init__.py
+++ packages/aspynotifications_rest/src/aspynotifications_rest/__init__.py
@@ -1,32 +1,13 @@
-from typing import Any, Dict, List, Optional
-
 import structlog
-
 from aspyconfig import get_config
+
 from aspynotifications_rest.domain.config.rest_server_config import (
     AspyNotificationsRestAppConfig,
 )
 
 logger = structlog.get_logger(__name__)
 
-_notifications_rest_app_config: Optional[AspyNotificationsRestAppConfig] = None
-
-
-def load_notifications_rest_config(
-    *,
-    cli_config: Optional[Dict[str, Any]] = None,
-    user_config_paths: Optional[List[str]] = None,
-    local_package_monoconfig: Optional[List[str]] = None,
-    app_defaults: Optional[Dict[str, Any]] = None,
-) -> None:
-    config = get_config()
-    config.register_common_config(
-        cli_config=cli_config,
-        local_config_paths=local_package_monoconfig,
-        user_config_paths=user_config_paths,
-        app_defaults=app_defaults,
-    )
-    config.load()
+_notifications_rest_app_config: AspyNotificationsRestAppConfig | None = None
 
 
 def get_notifications_rest_config() -> AspyNotificationsRestAppConfig:
@@ -39,4 +20,4 @@ def get_notifications_rest_config() -> AspyNotificationsRestAppConfig:
         _notifications_rest_app_config = model
         logger.debug("Notifications REST configuration loaded and validated")
 
-    return _notifications_rest_app_config
+    return _notifications_rest_app_config  # type: ignore
